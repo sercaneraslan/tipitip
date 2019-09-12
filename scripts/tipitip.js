4182531bcc0aedce10a7ea9b43810d0cc5b020cd
Sercan Eraslan <info@sercaneraslan.com>
2019-09-12 13:32:05 +0300

    Update tipitip.js

--- scripts/tipitip.js
+++ scripts/tipitip.js
@@ -1,31 +1,8 @@
 /*
-* Copyright 2013 Sahibinden Bilgi Teknolojileri Pazarlama ve Ticaret A.Ş.
 *
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
+* Tipitip.js v1.0
 *
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
 */
-
-/**
- *
- * Tipitip.js v1.0
- *
- * @sercaneraslan
- * @muratcorlu
- * 
- * @irfandurmus (http://irfandurmus.com) JSDoc'un ayrıştırabilmesi ve IDE'lerin tanıyabilmesi içi düzenleme yaptı
- *
- * @sahibinden.com 2013
- *
- */
 $(function() {
 
     var body = $("body"),
